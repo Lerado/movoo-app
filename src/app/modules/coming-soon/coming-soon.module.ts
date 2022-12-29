9f35eb5db9d501e26d90ca9fce6f9749f43a31a5
Darel Ngogang <darel.n@intekor.ca>
2022-12-29 23:11:14 +0100

    perf: images optimization
    Make use of NgOptimizedModule and image directive to optimize images

--- src/app/modules/coming-soon/coming-soon.module.ts
+++ src/app/modules/coming-soon/coming-soon.module.ts
@@ -3,11 +3,15 @@ import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 import { ComingSoonPageComponent } from './pages/coming-soon-page/coming-soon-page.component';
 import { comingSoonRoutes } from './coming-soon.routing';
+import { SharedModule } from 'app/shared/shared.module';
 
 @NgModule({
     imports: [
         RouterModule.forChild(comingSoonRoutes),
-        MatButtonModule
+
+        MatButtonModule,
+
+        SharedModule
     ],
     declarations: [
         ComingSoonPageComponent
